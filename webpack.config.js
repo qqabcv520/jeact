837a10cc134d29642bc7e99a73ed6fdf7aca9d48
mifan <605655316@qq.com>
2020-05-18 11:16:02 +0800

    chore: 输出模块改为umd

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js",
-        libraryTarget: 'var',
+        libraryTarget: 'umd',
     },
     devtool: 'source-map',
     //模块配置
